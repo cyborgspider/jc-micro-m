4f35ae759e65e9093626aaac2562e4bd0b82b9e7
Carlos Ortega <exoticorion@gmail.com>
2014-07-27 19:33:40 -0700

    second round of major commits

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,12 +1,22 @@
 (function() {
-  var animateIntroBar, animateIntroSlider, links;
+  var animateIntroBar, animateIntroSlider, animateMainIntroHeader, links, pageLoad, startSlider;
 
-  $('#main-intro-header').velocity('transition.slideLeftBigIn', {
-    duration: 800,
-    complete: function() {
-      return animateIntroBar();
-    }
-  });
+  pageLoad = function() {
+    return $('html,body').velocity('scroll', {
+      complete: function() {
+        return animateMainIntroHeader();
+      }
+    });
+  };
+
+  animateMainIntroHeader = function() {
+    return $('#main-intro-header').velocity('transition.slideLeftBigIn', {
+      duration: 800,
+      complete: function() {
+        return animateIntroBar();
+      }
+    });
+  };
 
   animateIntroBar = function() {
     return $('#main-intro-bar').velocity({
@@ -21,7 +31,11 @@
   };
 
   animateIntroSlider = function() {
-    return $('#main-intro-slider').velocity('transition.fadeIn');
+    return $('#main-intro-slider').velocity('transition.fadeIn', {
+      complete: function() {
+        return startSlider($('#main-intro-slider'));
+      }
+    });
   };
 
   links = $('.main-nav').find('a');
@@ -34,5 +48,47 @@
     links.removeClass('active');
     return $(this).addClass('active');
   });
+
+  startSlider = function($container) {
+    var $slideIndexContainer, $slides, $slidesLength, autoSlide, gotoSlide, i;
+    $slideIndexContainer = $container.find('.slider-index');
+    $slides = $container.find('p');
+    $slidesLength = $slides.length;
+    i = 1;
+    gotoSlide = function($slideIndex) {
+      var $this;
+      $this = $(this);
+      $slideIndex = $this.index();
+      i = $slideIndex;
+      $this.addClass('active');
+      $this.siblings().removeClass('active');
+      $slides.removeClass('active');
+      return $slides.eq($slideIndex).addClass('active');
+    };
+    autoSlide = function() {
+      if (i < $slidesLength) {
+        $slideIndexContainer.find('li:eq(' + i + ')').click();
+        return i++;
+      } else if (i = $slidesLength) {
+        return i = 0;
+      }
+    };
+    $slideIndexContainer.on('click', 'li', gotoSlide);
+    return setInterval(autoSlide, 3000);
+  };
+
+  $('.section').waypoint(function() {
+    var sectionId;
+    sectionId = $(this).attr('id');
+    $('#logo').removeClass('active');
+    links.removeClass('active');
+    return $('.main-nav').find('a[href="#' + sectionId + '"]').addClass('active');
+  });
+
+  $('#home').waypoint(function() {
+    return $('#logo').addClass('active');
+  });
+
+  pageLoad();
 
 }).call(this);
--- site/scripts/scripts.coffee
+++ site/scripts/scripts.coffee
@@ -1,10 +1,22 @@
-$('#main-intro-header').velocity(
-  'transition.slideLeftBigIn'
-  ,
-    duration: 800
-    complete: ->
-      animateIntroBar()
-)
+#Place all functions to init the page inside pageLoad
+pageLoad = ->
+  #If a user is at the bottom of the page and refreshes, scroll back to the top and start all animations
+  $('html,body').velocity(
+    'scroll'
+    ,
+      complete: ->
+        animateMainIntroHeader()
+  )
+
+#Place animations inside functions to call them using Velocity callbacks
+animateMainIntroHeader = ->
+  $('#main-intro-header').velocity(
+    'transition.slideLeftBigIn'
+    ,
+      duration: 800
+      complete: ->
+        animateIntroBar()
+  )
 
 animateIntroBar = ->
   $('#main-intro-bar').velocity(
@@ -17,10 +29,15 @@ animateIntroBar = ->
   )  
 
 animateIntroSlider = ->
-  $('#main-intro-slider').velocity 'transition.fadeIn'
+  $('#main-intro-slider').velocity(
+    'transition.fadeIn'
+  ,
+    complete: ->
+      startSlider($('#main-intro-slider'))
+  )
 
 
-#Navigation
+#Handle navigation
 links    = $('.main-nav').find('a')
 
 $('.main-nav').on 'click', 'a', (e) ->
@@ -29,3 +46,53 @@ $('.main-nav').on 'click', 'a', (e) ->
   $(section).velocity 'scroll'
   links.removeClass 'active'
   $(@).addClass 'active'
+
+
+#Slider
+startSlider = ($container) ->
+  $slideIndexContainer = $container.find('.slider-index')
+  $slides              = $container.find('p')
+  $slidesLength        = $slides.length
+  i                    = 1
+
+
+  gotoSlide = ($slideIndex) ->
+    $this       = $(@)
+    $slideIndex = $this.index()
+    i           = $slideIndex
+
+    $this.addClass 'active'
+    $this.siblings().removeClass 'active'
+
+    $slides.removeClass 'active'
+    $slides.eq($slideIndex).addClass 'active'
+
+
+  autoSlide = ->
+    #Increment i, the setInterval below will handle "auto" clicking
+    if i < $slidesLength
+      $slideIndexContainer.find('li:eq('+i+')').click()
+      i++ 
+    #At the end of the slideshow, reset to the first slide
+    else if i = $slidesLength
+      i = 0
+
+
+  $slideIndexContainer.on 'click', 'li', gotoSlide
+  setInterval(autoSlide, 3000)
+
+#Waypoints
+#http://imakewebthings.com/jquery-waypoints/#get-started
+$('.section').waypoint ->
+  sectionId = $(@).attr('id')
+  $('#logo').removeClass 'active'
+  links.removeClass 'active'
+
+  #Find out why I can't use the global variable I set earlier (links)
+  $('.main-nav').find('a[href="#'+sectionId+'"]').addClass('active')
+
+$('#home').waypoint ->
+  $('#logo').addClass 'active'
+
+#Page Load (init)
+pageLoad()
